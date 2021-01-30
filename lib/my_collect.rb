7b0837752cba602436bf51d74cef75d61240d090
Lena Hou <lenahou2013@gmail.com>
2021-01-30 22:48:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,4 +8,4 @@ def my_collect(collection)
   new_collection
 end
 
-my_collect(['ruby', 'javascript", 'python', 'objective-c']) { |lang| lang.upcase }
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |lang| lang.upcase }
