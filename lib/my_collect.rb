bfe12996c6b4a50d0556a1a4151ecce28ee835f8
Lena Hou <lenahou2013@gmail.com>
2021-01-30 22:42:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+  i = 0
+  while i < collection.length
+    new_collection << yield[i]
+    i += 1
+  end
+  new_collection
+end
 
+my_collection(["Tim Jones", "Tom Smith", "Jim Campagno"]) do {|name| name.split(" ").first } 
+    
 
